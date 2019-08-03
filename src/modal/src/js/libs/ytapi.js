fd65c37084a237046de1e7de63054115b66ab81b
afrmtbl <afrmtbl@gmail.com>
2019-08-03 16:21:57 -0400

    Updated ytapi to use background script for retrieving info

--- src/modal/src/js/libs/ytapi.js
+++ src/modal/src/js/libs/ytapi.js
@@ -2,80 +2,40 @@ const mode = process.env.NODE_ENV;
 const prod = "production";
 const dev = "development";
 
-const API_KEY = "AIzaSyBBdCo-KyDeWBHkR2JrGdeL7tyrMq3z1-4";
-
 export function getVideoData(videoId) {
-    instantview.log(`getting video data: videoId=${videoId}`);
-    return new Promise(async (resolve, reject) => {
-
-        const endpoint = `https://www.googleapis.com/youtube/v3/videos`;
-
-        try {
-            const response = await ytget(endpoint, {
-                id: videoId,
-                part: "snippet,statistics",
-                fields: "items(id,snippet(title,channelId,description,publishedAt),statistics(viewCount,likeCount,dislikeCount))"
-            });
-            if (response[0] && response[0].snippet) {
-                resolve(response[0]);
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({message: "get_video_data", videoId}, response => {
+            if (response.error) {
+                reject(response.error);
             }
-            else if (!response.length) {
-                reject(new Error(`There were no videos matching the id: ${videoId}`));
+            else {
+                resolve(response);
             }
-        }
-        catch(e) {
-            reject(e);
-        }
+        });
     });
 }
 export function getChannelData(channelId) {
-    instantview.log(`getting channel data: channelId=${channelId}`);
-    return new Promise(async (resolve, reject) => {
-
-        const endpoint = `https://www.googleapis.com/youtube/v3/channels`;
-
-        try {
-            const response = await ytget(endpoint, {
-                id: channelId,
-                part: "snippet,statistics",
-                fields: "items(id,snippet(title,thumbnails/default),statistics/subscriberCount)"
-            });
-            resolve(response[0]);
-        }
-        catch(e) {
-            reject(e);
-        }
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({message: "get_channel_data", channelId}, response => {
+            if (response.error) {
+                reject(response.error);
+            }
+            else {
+                resolve(response);
+            }
+        });
     });
 }
 export function getCommentData(videoId) {
-    instantview.log(`getting comment data: videoId=${videoId}`);
-    return new Promise(async (resolve, reject) => {
-
-        const endpoint = `https://www.googleapis.com/youtube/v3/commentThreads`;
-
-        try {
-            const field = "snippet/topLevelComment/snippet";
-            const response = await ytget(endpoint, {
-                videoId,
-                part: "snippet",
-                fields: `items(${field}/authorChannelUrl,${field}/authorDisplayName,${field}/textDisplay)`,
-                maxResults: 15,
-                order: "relevance"
-            });
-            const comments = response.map(comment => {
-                const data = comment.snippet.topLevelComment.snippet;
-                return {
-                    author: data.authorDisplayName,
-                    authorUrl: data.authorChannelUrl,
-                    text: data.textDisplay
-                };
-            });
-
-            resolve(comments);
-        }
-        catch(e) {
-            reject(e);
-        }
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({message: "get_comment_data", videoId}, response => {
+            if (response.error) {
+                reject(response.error);
+            }
+            else {
+                resolve(response);
+            }
+        });
     });
 }
 
@@ -216,35 +176,5 @@ export function getAuth(interactive = true) {
                 }
             });
         }
-    });
-}
-function ytget(endpoint, options, key = API_KEY) {
-    return new Promise((resolve, reject) => {
-
-        const url = new URL(endpoint);
-
-        for (const opt in options) {
-            url.searchParams.append(opt, options[opt]);
-        }
-
-        url.searchParams.append("key", key);
-
-        fetch(url).then(response => {
-            return response.json();
-        }).then(data => {
-            if (data.items) {
-                resolve(data.items);
-            }
-            else if (data.error) {
-                reject("Error while retrieving data from youtube: " + data.error.message + ` (code ${data.error.code})`);
-            }
-            else {
-                instantview.log("Error while retrieving data from youtube (ytget_e)");
-                reject("Error while retrieving data from youtube");
-            }
-        }).catch(e => {
-            instantview.log(`Error while retrieving data from youtube (ytget_c): e=${e.message}`);
-        });
-
     });
 }
