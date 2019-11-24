0e0f53c13b9f6d11e8de4286d44ad2c89390d955
afrmtbl <afrmtbl@gmail.com>
2019-11-23 22:59:31 -0500

    Build improvements

--- build_ext.sh
+++ build_ext.sh
@@ -1,1 +1,14 @@
-zip -r dist.zip _locales icons images libs manifest.json src/background.js src/background_video_info.js src/iv.js src/modal/dist/js/bundle.js src/options/dist
+# Remove any old builds
+rm dist.zip
+
+# Build the modal
+cd src/modal && npm run build
+
+# Build the options page
+cd ../options && npm run build
+
+# Get back to the root
+cd ../..
+
+# Zip the result
+zip -r dist.zip _locales icons images libs manifest.json src/background.js src/background_video_info.js src/datasource src/iv.js src/modal/dist/js/bundle.js src/options/dist
